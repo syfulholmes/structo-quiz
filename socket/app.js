42c35b0a6c718ba97835bb209d321b128059edc4
syfulholmes <syfulholmes@hotmail.com>
2018-06-05 12:59:18 +0800

    initial wireframe

---
+++ socket/app.js
@@ -0,0 +1,9 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+	res.send('Hello World!');
+})
+
+http.listen(8080);
